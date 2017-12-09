5ac16263671a4fa0295b4a3fb795b9c24e4be5f2
Kinimotto29 <dommin5@interia.pl>
2017-12-09 15:54:29 +0100

    progres2

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 $(function () {
-    
+
+    $('.blur').click (function () {
+        this.blur();
+    });
 
     $(window).scroll(function () {
-        
-        if($(window).scrollTop() > 1) {
+
+        if ($(window).scrollTop() > 1) {
             $('.navbar').css({
                 'background-color': 'rgba(14, 216, 214, 0.8)',
                 'color': '#363636;'
             });
             $('.nav-link').addClass('menu-grey');
             $('.navbar-brand').addClass('menu-grey');
-            
-            
+
+
         } else {
-             $('.navbar').css('background-color', 'transparent');
-             $('.nav-link').removeClass('menu-grey');
-             $('.navbar-brand').removeClass('menu-grey');
+            $('.navbar').css('background-color', 'transparent');
+            $('.nav-link').removeClass('menu-grey');
+            $('.navbar-brand').removeClass('menu-grey');
         }
 
     });
-
-
+    
 
 });
